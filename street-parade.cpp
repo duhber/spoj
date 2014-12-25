ddb724a3ab0cbc64fb2d0fdfebe663c072eaa292
duhber <duhbera@gmail.com>
2014-12-25 17:52:18 +0530

    add stree parade solution

--- street-parade.cpp
+++ street-parade.cpp
@@ -22,6 +22,39 @@ int main(){
         int a[n];
         for(int i=0;i<n;i++)
             scanf("%d",&a[i]);
+        stack<int> s;
+        int i=0;
+        while(a[i]!=1){
+            s.push(a[i]);
+            i++;
+        }
+        int prev=a[i];
+        i++;
+        for(;i<n;i++){
+            if(a[i]!=prev+1){
+                while(!s.empty() && s.top()==prev+1){
+                    prev=s.top();
+                    s.pop();
+                }
+                s.push(a[i]);                
+            }
+            else
+                prev=a[i];
+        }
+        string res="yes";
+        if(!s.empty()){
+            prev=s.top();
+            s.pop();
+        }
+        while(!s.empty()){
+            if(prev>s.top()){
+                res="no";
+                break;
+            }
+            prev=s.top();
+            s.pop();
+        }
+        cout<<res<<endl;
 
     }
     return 0;
