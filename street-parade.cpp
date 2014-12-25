de30a1413d027c1e492838078bec38f6e06d87be
duhber <duhbera@gmail.com>
2014-12-25 17:51:49 +0530

    add stree parade solution

---
+++ street-parade.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<algorithm>
+#include<list>
+#include<set>
+#include<cstring>
+#include<cstdio>
+//#include<unordered_map>
+#include<string>
+#include<limits>
+#include<functional>
+#include<queue>
+#include<stack>
+#include<utility>
+
+using namespace std;
+
+int main(){
+    int n;
+    while(scanf("%d",&n) && n!=0){
+        int a[n];
+        for(int i=0;i<n;i++)
+            scanf("%d",&a[i]);
+
+    }
+    return 0;
+    }
